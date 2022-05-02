6a51a6728ca59aead00367d87744711b29d4649a
shioyang <shioyang4321@gmail.com>
2022-05-02 11:56:03 +0900

    refactor: use Octokit

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,44 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { GitHub } from "@actions/github/lib/utils";
 
 async function run() {
-  const token = core.getInput("github-token", { required: true });
-  const client = new github.GitHub(token);
+    const token = core.getInput("github-token", { required: true });
+    const octokit = github.getOctokit(token);
 
-  const labelNames = await getPullRequestLabelNames(client);
+    const labelNames = await getPullRequestLabelNames(octokit);
 
-  const labels = getInputLabels();
-  const result = labels.every((label) => labelNames.includes(label));
-  core.setOutput("result", result);
+    const labels = getInputLabels();
+    const result = labels.every(
+        (label) => labelNames.findIndex((value) => label === value) >= 0
+    );
+    core.setOutput("result", result);
 }
 
 async function getPullRequestLabelNames(
-  client: github.GitHub
+    octokit: InstanceType<typeof GitHub>
 ): Promise<string[]> {
-  const owner = github.context.repo.owner;
-  const repo = github.context.repo.repo;
-  const commit_sha = github.context.sha;
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    const commit_sha = github.context.sha;
 
-  const response = await client.repos.listPullRequestsAssociatedWithCommit({
-    owner,
-    repo,
-    commit_sha,
-  });
+    const response =
+        await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+            owner,
+            repo,
+            commit_sha,
+        });
 
-  const pr = response.data.length > 0 && response.data[0];
-  return pr ? pr.labels.map((label) => label.name) : [];
+    const pr = response.data.length > 0 && response.data[0];
+    return pr ? pr.labels.map((label) => label.name || "") : [];
 }
 
 function getInputLabels(): string[] {
-  const raw = core.getInput("labels", { required: true });
-  const json = JSON.parse(raw);
-  return Array.isArray(json) ? json : [];
+    const raw = core.getInput("labels", { required: true });
+    const json = JSON.parse(raw);
+    return Array.isArray(json) ? json : [];
 }
 
 run().catch((err) => {
-  core.setFailed(err.message);
+    core.setFailed(err.message);
 });
