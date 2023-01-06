0ead67097e9f5dcc704e9a02bfa8de3aa4e379c5
Tomohiro (Tomo) SHIOYA <shioyang4321@gmail.com>
2023-01-06 11:01:04 +0900

    refactor: update file formats (#24)
    * refactor: update format
    
    * refactor: decrease tab width to 2
    
    * chore: prettier config for yml
    
    * refactor: format

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,42 @@ import * as github from "@actions/github";
 import { GitHub } from "@actions/github/lib/utils";
 
 async function run() {
-    const token = core.getInput("github-token", { required: true });
-    const octokit = github.getOctokit(token);
+  const token = core.getInput("github-token", { required: true });
+  const octokit = github.getOctokit(token);
 
-    const labelNames = await getPullRequestLabelNames(octokit);
+  const labelNames = await getPullRequestLabelNames(octokit);
 
-    const labels = getInputLabels();
-    const result = labels.every(
-        (label) => labelNames.findIndex((value) => label === value) >= 0
-    );
-    core.setOutput("result", result);
+  const labels = getInputLabels();
+  const result = labels.every(
+    (label) => labelNames.findIndex((value) => label === value) >= 0
+  );
+  core.setOutput("result", result);
 }
 
 async function getPullRequestLabelNames(
-    octokit: InstanceType<typeof GitHub>
+  octokit: InstanceType<typeof GitHub>
 ): Promise<string[]> {
-    const owner = github.context.repo.owner;
-    const repo = github.context.repo.repo;
-    const commit_sha = github.context.sha;
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  const commit_sha = github.context.sha;
 
-    const response =
-        await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-            owner,
-            repo,
-            commit_sha,
-        });
+  const response =
+    await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+      owner,
+      repo,
+      commit_sha,
+    });
 
-    const pr = response.data.length > 0 && response.data[0];
-    return pr ? pr.labels.map((label) => label.name || "") : [];
+  const pr = response.data.length > 0 && response.data[0];
+  return pr ? pr.labels.map((label) => label.name || "") : [];
 }
 
 function getInputLabels(): string[] {
-    const raw = core.getInput("labels", { required: true });
-    const json = JSON.parse(raw);
-    return Array.isArray(json) ? json : [];
+  const raw = core.getInput("labels", { required: true });
+  const json = JSON.parse(raw);
+  return Array.isArray(json) ? json : [];
 }
 
 run().catch((err) => {
-    core.setFailed(err.message);
+  core.setFailed(err.message);
 });
